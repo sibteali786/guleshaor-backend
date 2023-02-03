8c51c23745bf7bcb1481bb17f2cc768047bb1ac0
Syed_Sibteali_Baqar <sibteali786@gmail.com>
2023-02-03 09:21:16 +0500

    working fine now

--- api/mentorRoutes.js
+++ api/mentorRoutes.js
@@ -6,7 +6,6 @@ const {
 
 // async routes have promises returned by the schema of the db
 const router = express.Router();
-
 router.route("/").get(getMentors);
 router.route("/:id").get(getMentorById);
 module.exports = router;
--- api/userRoutes.js
+++ api/userRoutes.js
@@ -3,13 +3,17 @@ const {
   authUser,
   getUserProfile,
   registerUser,
+  updateUserProfile,
 } = require("../controllers/userController.js");
 const { protect } = require("../middleware/authMiddleware.js");
 const router = express.Router();
 // async routes have promises returned by the schema in the database
 router.route("/").post(registerUser);
 router.post("/login", authUser);
-router.route("/profile").get(protect, getUserProfile);
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 module.exports = router;
 
 // TODO:https://gabrieleromanato.name/nodejs-create-a-simple-social-network-with-expressjs
