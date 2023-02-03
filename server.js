be4543ba36564c41ef7dcb79f0dc246489aa166f
Syed_Sibteali_Baqar <sibteali786@gmail.com>
2023-02-03 05:04:15 +0500

    server js changes

--- server.js
+++ server.js
@@ -48,12 +48,13 @@ app.use("/uploads", express.static(path.join(path.resolve(), "/uploads"))); // *
 app.use(notFound);
 app.use(errorHandler);
 
-const __dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.use(express.static(path.join(path.resolve(), "/frontend/build")));
 
   app.get("/*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+    res.sendFile(
+      path.resolve(path.resolve(), "frontend", "build", "index.html")
+    )
   );
 } else {
   app.get("/", (req, res) => {
