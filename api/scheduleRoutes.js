1e685bf394b83615e536f3a650112353555d015d
Syed_Sibteali_Baqar <sibteali786@gmail.com>
2023-05-04 10:58:21 +0500

    GS-88 1. Added deleteAll schedules api endpoint. -> Tested with postman at localhost:5000/api/schedule/deleteAll/userId

--- api/scheduleRoutes.js
+++ api/scheduleRoutes.js
@@ -6,10 +6,12 @@ const {
   getSchedule,
   updateSchedule,
   deleteSchedule,
+  deleteAllSchedule,
 } = require("../controllers/scheduleController");
 router.route("/create").post(protect, createSchedule);
 router.route("/:userId").get(protect, getSchedule);
 router.route("/:scheduleId").put(protect, updateSchedule);
 router.route("/:scheduleId").delete(protect, deleteSchedule);
+router.route("/deleteAll/:userId").delete(protect, deleteAllSchedule);
 
 module.exports = router;
